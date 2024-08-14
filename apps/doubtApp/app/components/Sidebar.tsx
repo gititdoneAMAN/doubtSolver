df70ed190e94dfcd34fdd4cfdb3f354a7fb02d82
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-08-14 23:56:22 +0530

    Made the sidebar dynamic

---
+++ apps/doubtApp/app/components/Sidebar.tsx
@@ -0,0 +1,176 @@
+"use client";
+import React, { useState } from "react";
+import { SidebarItem } from "./SidebarItem";
+import { getServerSession } from "next-auth";
+import { authConfig } from "../../lib/AuthConfig";
+import { useSession } from "next-auth/react";
+
+const Sidebar = () => {
+  const session = useSession();
+  const [size, setSize] = useState(72);
+  const [homeContent, setHomeContent] = useState("Home");
+  const [askContent, setAskContent] = useState("Ask");
+  const [solveContent, setSolveContent] = useState("Solve");
+  const [searchContent, setSearchContent] = useState("Search");
+  const [profileContent, setProfileContent] = useState("Profile");
+  const [askedContent, setAskedContent] = useState("Asked");
+  return (
+    <div>
+      <div
+        className={`w-${size} pr-10 border-r border-white min-h-screen pt-28 flex flex-col gap-2`}
+      >
+        <SidebarItem href="/feed" title={homeContent} icon={<FeedIcon />} />
+        {session?.data?.user?.role === "Student" ? (
+          <SidebarItem href="/ask" title={askContent} icon={<AskIcon />} />
+        ) : (
+          <SidebarItem href="/solve" title={solveContent} icon={<AskIcon />} />
+        )}
+        <SidebarItem
+          href="/search"
+          title={searchContent}
+          icon={<SearchIcon />}
+        />
+        <SidebarItem
+          href="/profile"
+          title={profileContent}
+          icon={<ProfileIcon />}
+        />
+        {session?.data?.user?.role === "Student" ? (
+          <SidebarItem
+            href="/asked"
+            title={askedContent}
+            icon={<AskedLogo />}
+          />
+        ) : (
+          <></>
+        )}
+        <div
+          className="my-auto p-8 cursor-pointer"
+          onClick={() => {
+            setSize(20);
+            setHomeContent("");
+            setAskContent("");
+            setSolveContent("");
+            setSearchContent("");
+            setProfileContent("");
+            setAskedContent("");
+          }}
+        >
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-8 bg-white rounded-full p-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"
+              />
+            </svg>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
+
+function FeedIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+      />
+    </svg>
+  );
+}
+
+function AskIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+      />
+    </svg>
+  );
+}
+
+function SearchIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+      />
+    </svg>
+  );
+}
+
+function ProfileIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+      />
+    </svg>
+  );
+}
+
+function AskedLogo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+      />
+    </svg>
+  );
+}
